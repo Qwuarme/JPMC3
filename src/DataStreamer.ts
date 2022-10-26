40ce2fe1ab7c433843183ea726f21582bef8d040
Eugene Osei-Adjapong <eugeneosei-adjapong@Qwuarmes-MacBook-Pro.local>
2022-10-26 06:37:16 +0000

    JPM

--- src/DataStreamer.ts
+++ src/DataStreamer.ts
@@ -1,12 +1,12 @@
 export interface Order {
-  price: number,
-  size: number,
+  price:number,
+  size:number,
 }
 export interface ServerRespond {
-  stock: string,
-  top_bid: Order,
-  top_ask: Order,
-  timestamp: Date,
+  stock:string,
+  top_bid:Order,
+  top_ask:Order,
+  timestamp:Date,
 }
 
 class DataStreamer {
